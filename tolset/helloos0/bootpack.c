0592595ccb05ba37ee7d9622ab5a7b6fe401d18e
roliygu <roliygu@gmail.com>
2014-07-12 18:13:43 +0800

    V2.1.1

--- tolset/helloos0/bootpack.c
+++ tolset/helloos0/bootpack.c
@@ -1,7 +1,14 @@
 void io_hlt(void);
+void write_mem8(int addr, int data);
 
 void HariMain(void){
-	fin:
+	int i;
+	for(i = 0xa0000; i<=0xaffff; i+=2){
+		write_mem8(i, 15);
+		if(i%4==0)
+			write_mem8(i, 7);
+	}
+	for(;;){
 		io_hlt();
-		goto fin; 
+	}
 }
