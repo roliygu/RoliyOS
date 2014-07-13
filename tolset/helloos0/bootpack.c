841ddf4ead580c3f450473d1f2052b077d497db2
roliygu <roliygu@gmail.com>
2014-07-13 14:06:21 +0800

    V2.1.2

--- tolset/helloos0/bootpack.c
+++ tolset/helloos0/bootpack.c
@@ -4,9 +4,8 @@ void write_mem8(int addr, int data);
 void HariMain(void){
 	int i;
 	for(i = 0xa0000; i<=0xaffff; i+=2){
-		write_mem8(i, 15);
-		if(i%4==0)
-			write_mem8(i, 7);
+		char *p= (char *)i;
+		*p = i%4;
 	}
 	for(;;){
 		io_hlt();
--- tolset/helloos0/naskfunc.nas
+++ tolset/helloos0/naskfunc.nas
@@ -13,9 +13,4 @@
 _io_hlt:	; void io_hlt(void);
 		HLT
 		RET
-
-_write_mem8:	; void write_mem8(int addr, int data);
-		MOV		ECX,[ESP+4]		; [ESP+4]存放的是地址
-		MOV		AL,[ESP+8]		; [ESP+8]存放的是数值
-		MOV		[ECX],AL
-		RET
+		
